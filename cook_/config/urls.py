26ac6464b818a8831674e187300ed298eda1c235
steepHaker <andrejmagkoev585@gmail.com>
2023-06-26 07:46:32 +0300

    Update urls.py

--- cook_/config/urls.py
+++ cook_/config/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
     path('', include('blog.urls')),
 ]
 
