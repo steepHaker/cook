7f005b76698a51860bf365837e57e39f302de9f3
steepHaker <andrejmagkoev585@gmail.com>
2023-06-27 03:30:30 +0300

    Update menu_tags.py

--- cook_/blog/templatetags/menu_tags.py
+++ cook_/blog/templatetags/menu_tags.py
@@ -12,5 +12,5 @@ def get_categories():
 
 @register.inclusion_tag('blog/include/tags/recipes_tag.html')
 def get_last_posts():
-    posts = Post.objects.select_related("category").order_by("-id")[:10]
+    posts = Post.objects.select_related("category").order_by("-id")[:5]
     return {"list_last_post": posts}
